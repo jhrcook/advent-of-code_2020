5153aac7e89ae27aec21438f9b57217e7cb12043
Josh Cook <joshuacook0023@gmail.com>
2020-12-06 08:02:50 -0500

    challenge 6 part 2 solution

--- challenges/06_challenge.py
+++ challenges/06_challenge.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 import colorama as clr
-import numpy as np
 
 # Prepare colorama to highlight printed solutions.
 clr.init(autoreset=True)
@@ -9,18 +8,28 @@ answer_highlight = clr.Fore.BLUE + clr.Style.BRIGHT
 
 # Read in data.
 input_data_path = Path("data/day_06/input.txt")
-customs_answers = []
-answer = set([])
+customs_answers = []  # List of all answers.
+answer = set([])  # One groups answers.
+is_new_group = True  # Track if there is a new group.
 with open(input_data_path, "r") as file:
     for x in file:
         if x == "\n":
+            # Reset for the next group.
             customs_answers.append(answer)
             answer = set([])
+            is_new_group = True
         else:
-            answer = answer.union(set(x.rstrip()))
+            # Add new person's answers to the current group's answer.
+            if is_new_group:
+                answer = set(x.rstrip())
+                is_new_group = False
+            else:
+                answer = answer.intersection(set(x.rstrip()))
+
 # Add on the last one.
 customs_answers.append(answer)
 
+# Print results.
 print(f"number of groups: {len(customs_answers)}")
 print(
     answer_highlight
