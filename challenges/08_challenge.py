8560975a5947ff02032baac27f1ea406e863344d
Josh Cook <joshuacook0023@gmail.com>
2020-12-08 10:39:31 -0500

    challenge 8 part 2 solution

--- challenges/08_challenge.py
+++ challenges/08_challenge.py
@@ -33,27 +33,70 @@ with open(input_data_path, "r") as file:
         code.append(Code(l.rstrip()))
 
 
-current_line = 0
-total_accumulated = 0
+def run_code(code):
+    code_finished = True
+    current_line = 0
+    total_accumulated = 0
 
-# Game loop
-while True:
-    line = code[current_line]
+    while True:
+        # Check the line of code.
+        if current_line == len(code):
+            code_finished = True
+            break
+        elif current_line > len(code):
+            code_finished = False
+            break
 
-    if line.has_been_executed:
-        # Stop if the code has been run before.
-        break
-    elif line.op == "nop":
-        # No operation.
-        current_line += 1
-    elif line.op == "acc":
-        # Add value to to accumulate.
-        total_accumulated += line.arg
-        current_line += 1
-    elif line.op:
-        # Jump.
-        current_line += line.arg
+        line = code[current_line]
 
-    line.has_been_executed = True
+        # Game loop.
+        if line.has_been_executed:
+            # Stop if the code has been run before.
+            code_finished = False
+            break
+        elif line.op == "nop":
+            # No operation.
+            current_line += 1
+        elif line.op == "acc":
+            # Add value to to accumulate.
+            total_accumulated += line.arg
+            current_line += 1
+        elif line.op:
+            # Jump.
+            current_line += line.arg
 
-print(answer_highlight + f"Final aggregate: {total_accumulated}")
+        line.has_been_executed = True
+
+    return (total_accumulated, current_line, code_finished)
+
+
+def swap_operation(l):
+    """Swap the operation from 'jmp' <--> 'nop' for a line of code."""
+    if l.op == "jmp":
+        l.op = "nop"
+    elif l.op == "nop":
+        l.op = "jmp"
+    else:
+        return False
+    return True
+
+
+# Try changing each line of code and seeing if the program finishes.
+for change_line_i in range(len(code)):
+
+    # Reset the `has_been_executed` attribute for each line.
+    for l in code:
+        l.has_been_executed = False
+
+    # Change the line of code if it is "jmp" or "op".
+    # (returns whether or not the code was changed)
+    code_did_change = swap_operation(code[change_line_i])
+
+    if code_did_change:
+        res = run_code(code)
+        swap_operation(code[change_line_i])
+        if res[2]:
+            print(
+                answer_highlight
+                + f"changed line {change_line_i} to finish with total: {res[0]}"
+            )
