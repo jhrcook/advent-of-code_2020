81851c682419eae400e639a5a979482e4d5c8ba2
Josh Cook <joshuacook0023@gmail.com>
2020-12-11 23:21:35 -0500

    challenge 11 part 2 solution

--- README.md
+++ README.md
@@ -17,7 +17,7 @@
 8. [Day 8](challenges/08_challenge.py) ⭐️⭐️
 9. [Day 9](challenges/09_challenge.py) ⭐️⭐️
 10. [Day 10](challenges/10_challenge.py) ⭐️⭐️ (#2 is very difficult 🥵)
-11. [Day 11](challenges/11_challenge.py)
+11. [Day 11](challenges/11_challenge.py) ⭐️⭐️ (#2 is very tedious)
 12. [Day 12](challenges/12_challenge.py)
 13. [Day 13](challenges/13_challenge.py)
 14. [Day 14](challenges/14_challenge.py)
--- challenges/11_challenge.py
+++ challenges/11_challenge.py
@@ -18,18 +18,74 @@ with open(input_data_path, "r") as file:
 seats = np.asarray(seats)
 
 
-def get_plausible_range(a, min_a, max_a):
-    return list(range(max(min_a, a - 1), min(max_a, a + 2)))
+def get_above(i, j, ary):
+    return np.flip(ary[:i, j])
+
+
+def get_below(i, j, ary):
+    return ary[i + 1 :, j]
+
+
+def get_left(i, j, ary):
+    return np.flip(ary[i, :j])
+
+
+def get_right(i, j, ary):
+    return ary[i, (j + 1) :]
+
+
+def get_lr_diag_values(i, j, ary):
+    """Get the values along the left-right (standard) diagonal."""
+    rows, cols = np.indices(ary.shape)
+    row_idx = np.diag(rows, k=j - i)
+    col_idx = np.diag(cols, k=j - i)
+    return ary[row_idx, col_idx]
+
+
+def get_upleft(i, j, ary):
+    return np.flip(get_lr_diag_values(i, j, ary)[: min(i, j)])
+
+
+def get_dnright(i, j, ary):
+    return get_lr_diag_values(i, j, ary)[(min(i, j) + 1) :]
+
+
+def get_rl_diag_values(i, j, ary):
+    """Get the values along the right-left diagonal."""
+    new_j = (ary.shape[1] - 1) - j
+    rows, cols = np.indices(ary.shape)
+    row_idx = np.diag(rows, k=new_j - i)
+    col_idx = np.diag(cols, k=new_j - i)
+    return np.flip(np.fliplr(ary)[row_idx, col_idx])
+
+
+def get_dnleft(i, j, ary):
+    return np.flip(get_rl_diag_values(i, j, ary)[: min(j, ary.shape[0] - 1 - i)])
+
+
+def get_upright(i, j, ary):
+    return get_rl_diag_values(i, j, ary)[(min(j, ary.shape[0] - 1 - i) + 1) :]
 
 
 def get_neighbors(i, j, ary):
     """Get the neighbors for seat a (i, j)."""
     neighbors = []
-    i_range = get_plausible_range(i, 0, ary.shape[0])
-    j_range = get_plausible_range(j, 0, ary.shape[1])
-    for a, b in product(i_range, j_range):
-        if i != a or j != b:
-            neighbors.append(ary[a, b])
+
+    for f in [
+        get_above,
+        get_below,
+        get_left,
+        get_right,
+        get_upleft,
+        get_upright,
+        get_dnleft,
+        get_dnright,
+    ]:
+        s = f(i, j, ary)
+        s = s[s != "."]
+        if len(s) > 0:
+            neighbors.append(s[0])
+
     return np.array(neighbors)
 
 
@@ -57,11 +113,11 @@ while not np.all(seats_0 == seats_1):
             if seats_0[i, j] == "L" and n_neighbors == 0:
                 # Seat becomes taken.
                 seats_1[i, j] = "#"
-            elif seats_0[i, j] == "#" and n_neighbors >= 4:
+            elif seats_0[i, j] == "#" and n_neighbors >= 5:
                 # Seat becomes empty.
                 seats_1[i, j] = "L"
 
-print(f"seats converged at step: {count}")  # 98
+print(f"seats converged at step: {count}")  # takes 86 steps
 num_seats_occupied = np.sum(seats_0 == "#")
 print(
     answer_highlight + f"number of seats occupied at equilibrium: {num_seats_occupied}"
