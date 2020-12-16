96f3d502fe6766e15fe44ab870c36ff0929a583a
Josh Cook <joshuacook0023@gmail.com>
2020-12-15 21:26:39 -0500

    challenge 15 part 2 solution

--- challenges/15_challenge.py
+++ challenges/15_challenge.py
@@ -1,6 +1,5 @@
-from itertools import product
 from pathlib import Path
-from typing import List
+from time import time
 
 import colorama
 import numpy as np
@@ -9,20 +8,40 @@ import numpy as np
 colorama.init(autoreset=True)
 answer_highlight = colorama.Fore.BLUE + colorama.Style.BRIGHT
 
-
 # Input data.
 input_data = "12,1,16,3,11,0"
-values = np.array([int(x) for x in input_data.split(",")])
+values = [int(x) for x in input_data.split(",")]
 
-for turn in range(len(values) + 1, 2020 + 1):
-    print(f"turn: {turn}", end="\r")
-    previous_val = values[turn - 2]
-    locs = np.where(values == previous_val)[0]
-    if locs.shape[0] == 1:
-        values = np.append(values, 0)
-    else:
-        locs = locs - 1
-        next_val = locs[-1] - locs[-2]
-        values = np.append(values, next_val)
+n_turns = 30000000
+starting_turn = len(values) + 1
 
-print(answer_highlight + f"2020th answer: {values[-1]}")
+# Dictionary to hold at what index a value was previously used.
+previous_idx = {}  # [value: idx]
+# Dictionary to hold at what index a value was last used.
+last_idx = {val: idx + 1 for idx, val in enumerate(values)}  # [value: idx]
+# The value used in the previous turn. (updated each turn)
+previous_value = values[-1]
+
+# Start timer.
+start_time = time()
+
+for turn in range(starting_turn, n_turns + 1):
+    try:
+        new_value = last_idx[previous_value] - previous_idx[previous_value]
+    except:
+        new_value = 0
+
+    try:
+        previous_idx[new_value] = last_idx[new_value]
+    except:
+        None
+
+    last_idx[new_value] = turn
+    previous_value = new_value
+
+# End timer.
+finish_time = time()
+
+print("")
+print(f"operation took {(finish_time - start_time) / 60:.2f} min.")
+print(answer_highlight + f"{n_turns}th answer: {previous_value}")
