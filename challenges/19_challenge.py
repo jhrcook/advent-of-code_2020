b4e0af774f029f31d2f10ed77ceac7d14789a230
Josh Cook <joshuacook0023@gmail.com>
2020-12-20 09:04:51 -0800

    challenge 19 part 2 solution

--- challenges/19_challenge.py
+++ challenges/19_challenge.py
@@ -1,3 +1,4 @@
+import re
 from itertools import product
 from pathlib import Path
 
@@ -8,16 +9,9 @@ import numpy as np
 colorama.init(autoreset=True)
 answer_highlight = colorama.Fore.BLUE + colorama.Style.BRIGHT
 
-DEBUG = False
-if DEBUG:
-    input_data_file = Path("data", "day_19", "input_test.txt")
-    n_cycles = 6
-else:
-    input_data_file = Path("data", "day_19", "input.txt")
-    n_cycles = 6
-
 
 # Input data.
+input_data_file = Path("data", "day_19", "input.txt")
 rules = []
 messages = []
 collecting_rules = True
@@ -53,9 +47,40 @@ print(f"number of rules: {len(rules)}")
 print(f"number of messages: {len(messages)}")
 
 
+def compile_regex(x, n_times="+"):
+    """Turn a list into a grouped regular expression."""
+    y = "(" + "|".join(x) + ")" + n_times
+    return y
+
+
 def add_rule_at(i, rules, messages):
     rule = rules[i]
     new_messages = []
+
+    if i == 8:
+        new_messages = []
+        regex_42 = compile_regex(add_rule_at(42, rules=rules, messages=[]))
+        if len(messages) == 0:
+            new_messages = [regex_42]
+        else:
+            new_messages = [message + regex_42 for message in messages]
+        return new_messages
+    elif i == 11:
+        new_messages = []
+        # Add '{N}' instead of '+' so can be replaced by actual numbers during matching.
+        regex_42 = compile_regex(
+            add_rule_at(42, rules=rules, messages=[]), n_times="{N}"
+        )
+        regex_31 = compile_regex(
+            add_rule_at(31, rules=rules, messages=[]), n_times="{N}"
+        )
+
+        if len(messages) == 0:
+            new_messages = [regex_42 + regex_31]
+        else:
+            new_messages = [regex_42 + m + regex_31 for m in messages]
+        return new_messages
+
     for values in rule:
         messages_copy = messages.copy()
         if values == ["a"] or values == ["b"]:
@@ -73,5 +98,16 @@ def add_rule_at(i, rules, messages):
 all_possible_messages = add_rule_at(0, rules=rules, messages=[])
 print(f"num possibles: {len(set(all_possible_messages))}")
 
-valid_messages = set(messages).intersection(set(all_possible_messages))
+# Add in start and end regex indicators.
+all_possible_messages = ["^" + m + "$" for m in all_possible_messages]
+
+valid_messages = []
+for message in messages:
+    for possible_message in all_possible_messages:
+        for N in range(1, 10):
+            possible_message_N = possible_message.replace("N", str(N))
+            if bool(re.match(possible_message_N, message)):
+                valid_messages.append(message)
+                break
+
 print(answer_highlight + f"number of valid messages: {len(valid_messages)}")
